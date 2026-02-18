459495b34b408f2a395268f35563f52e4a917677
Mohd Shaad Siddiqui <shaadstudy2027@gmail.com>
2026-02-18 21:33:29 +0530

    Time: 5 ms (17.02%), Space: 44.3 MB (37.99%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -1,32 +1,40 @@
 class Solution {
+
+    class Pair {
+        TreeNode node;
+        int row;
+        int col;
+
+        Pair(TreeNode node, int row, int col) {
+            this.node = node;
+            this.row = row;
+            this.col = col;
+        }
+    }
+
     public List<List<Integer>> verticalTraversal(TreeNode root) {
         List<List<Integer>> ans = new ArrayList<>();
         if (root == null) return ans;
 
-        // column -> list of (row, value)
         Map<Integer, List<int[]>> map = new TreeMap<>();
+        Queue<Pair> queue = new LinkedList<>();
 
-        Queue<Object[]> queue = new LinkedList<>();
-        queue.offer(new Object[]{root, 0, 0}); // node, row, col
+        queue.offer(new Pair(root, 0, 0));
 
         while (!queue.isEmpty()) {
-            Object[] arr = queue.poll();
-            TreeNode node = (TreeNode) arr[0];
-            int row = (int) arr[1];
-            int col = (int) arr[2];
+            Pair p = queue.poll();
 
-            map.putIfAbsent(col, new ArrayList<>());
-            map.get(col).add(new int[]{row, node.val});
+            map.putIfAbsent(p.col, new ArrayList<>());
+            map.get(p.col).add(new int[]{p.row, p.node.val});
 
-            if (node.left != null)
-                queue.offer(new Object[]{node.left, row + 1, col - 1});
+            if (p.node.left != null)
+                queue.offer(new Pair(p.node.left, p.row + 1, p.col - 1));
 
-            if (node.right != null)
-                queue.offer(new Object[]{node.right, row + 1, col + 1});
+            if (p.node.right != null)
+                queue.offer(new Pair(p.node.right, p.row + 1, p.col + 1));
         }
 
         for (List<int[]> list : map.values()) {
-            // sort by row first, then by value
             Collections.sort(list, (a, b) -> {
                 if (a[0] == b[0])
                     return a[1] - b[1];
@@ -34,8 +42,8 @@ class Solution {
             });
 
             List<Integer> column = new ArrayList<>();
-            for (int[] pair : list) {
-                column.add(pair[1]);
+            for (int[] arr : list) {
+                column.add(arr[1]);
             }
             ans.add(column);
         }
