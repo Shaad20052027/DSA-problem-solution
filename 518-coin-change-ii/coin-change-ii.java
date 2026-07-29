87cc234340578adc4b3e2103ba9e850acb98ef17
Mohd Shaad Siddiqui <shaadstudy2027@gmail.com>
2026-07-29 10:20:58 +0530

    Time: 11 ms (60.86%) | Memory: 57.4 MB (11.46%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -6,13 +6,13 @@ class Solution {
             dp[0][t] = (t % coins[0] == 0) ? 1 : 0;
         }
         for(int idx = 1; idx < n; idx++){
-            for(int target = 0; target <= amount; target++){
-                int notake = dp[idx - 1][target];
+            for(int t = 0; t <= amount; t++){
+                int notake = dp[idx - 1][t];
                 int take = 0;
-                if(coins[idx] <= target){
-                    take = dp[idx][target - coins[idx]];
+                if(coins[idx] <= t){
+                    take = dp[idx][t - coins[idx]];
                 }
-                dp[idx][target] = take + notake;
+                dp[idx][t] = take + notake;
             }
         }
         return dp[n - 1][amount];
